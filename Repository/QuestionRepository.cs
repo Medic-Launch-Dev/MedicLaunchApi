8b90a79314a5092cff2848a2b919ef3c59aa8b1a
Khalid Abdilahi <kabdilahi@microsoft.com>
2023-12-23 15:43:18 -0800

    in progress

---
+++ Repository/QuestionRepository.cs
@@ -0,0 +1,15 @@
+﻿using MedicLaunchApi.Models;
+using MedicLaunchApi.Storage;
+
+namespace MedicLaunchApi.Repository
+{
+    public class QuestionRepository
+    {
+        private readonly AzureBlobClient azureBlobClient;
+
+        public QuestionRepository(AzureBlobClient azureBlobClient)
+        {
+            this.azureBlobClient = azureBlobClient;
+        }
+    }
+}
---
+++ Repository/UserRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MedicLaunchApi.Repository
+{
+    public abstract class UserRepositoryBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public UserRepositoryBase(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+    }
+}
