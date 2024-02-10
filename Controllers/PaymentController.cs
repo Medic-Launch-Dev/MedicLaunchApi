a670b91a40cf6b7e0ee0b6ea54e3fb4460b9218e
Khalid Abdilahi <kabdilahi@microsoft.com>
2024-02-10 13:28:22 -0800

    add payment

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace MedicLaunchApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly string stripeApiKey;
+        private readonly string stripePublishableKey;
+        public PaymentController()
+        {
+            this.stripeApiKey = Environment.GetEnvironmentVariable("STRIPE_API_KEY") ?? string.Empty;
+            if (string.IsNullOrEmpty(this.stripeApiKey))
+            {
+                throw new Exception("STRIPE_API_KEY environment variable is not set");
+            }
+
+            this.stripePublishableKey = Environment.GetEnvironmentVariable("STRIPE_PUBLISHABLE_KEY") ?? string.Empty;
+            if (string.IsNullOrEmpty(this.stripePublishableKey))
+            {
+                throw new Exception("STRIPE_PUBLISHABLE_KEY environment variable is not set");
+            }
+        }
+
+        [HttpPost]
+        [Route("create-payment-intent")]
+        public IActionResult CreatePaymentIntent(string paymentPlan)
+        {
+            // TODO: Add logic to determine the amount based on the payment plan
+
+            StripeConfiguration.ApiKey = this.stripeApiKey;
+
+            var options = new PaymentIntentCreateOptions
+            {
+                Amount = 2000,
+                Currency = "GBP", // TODO: Change to the currency of the user
+                AutomaticPaymentMethods = new PaymentIntentAutomaticPaymentMethodsOptions
+                {
+                    Enabled = true,
+                },
+            };
+            var service = new PaymentIntentService();
+            PaymentIntent paymentIntent = service.Create(options);
+            return Ok(new { clientSecret = paymentIntent.ClientSecret });
+        }
+
+        [HttpGet]
+        [Route("publishable-key")]
+        public IActionResult GetPublishableKey()
+        {
+            return Ok(new { publishableKey = this.stripePublishableKey });
+        }
+    }
+}
