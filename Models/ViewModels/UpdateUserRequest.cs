e6c637696df83cc26891effc2fbd0818a587a567
Khalid Abdilahi <kabdilahi@microsoft.com>
2024-04-20 16:44:21 -0700

    User management - not tested yet

---
+++ Models/ViewModels/UpdateUserRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace MedicLaunchApi.Models.ViewModels
+{
+    public class UpdateUserRequest
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Email { get; set; }
+
+        public string University { get; set; }
+
+        public int GraduationYear { get; set; }
+
+        public string City { get; set; }
+
+    }
+}
---
+++ Models/ViewModels/UserProfileModel.cs
@@ -0,0 +1,25 @@
+﻿namespace MedicLaunchApi.Models.ViewModels
+{
+    public class UserProfileModel
+    {
+        public string Id { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Email { get; set; }
+
+        public string University { get; set; }
+
+        public int GraduationYear { get; set; }
+
+        public string City { get; set; }
+
+        public bool SubscribeToPromotions { get; set; }
+
+        public string SubscriptionMonths { get; set; }
+
+        public string SubscriptionPurchaseDate { get; set; }
+
+        public int QuestionsCompleted { get; set; }
+    }
+}
